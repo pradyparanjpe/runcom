3e9d9dafbac4e9efafdf3816d9a63e9d9cb5156c
Pradyumna Paranjape <pradyparanjpe@rediffmail.com>
2022-12-02 16:46:05 +0530

    better alternatives function,
    better alternatives: additions: atuin, delta, dust, lfs
    docstrings for shell functions
    nethogs through sudo instead of su
    setup: experimental: brew [Failing]
    publish.el: Ready for gitlab-ci

--- publish.el
+++ publish.el
@@ -1,7 +1,28 @@
-(require 'ox-man)
+;; publish.el --- Publish runcom on Gitlab Pages
+;; Author: Pradyumna Paranjape
+
+;;; Commentary:
+;; This script will tangle and export org mode files to executables and
+;; html pages.
+
+;;; Code:
+
+;; Org mode and melpa repos
+(when (getenv "CI_PROJECT_ID")
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("elpa" .  "https://elpa.gnu.org/packages/" ) t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents)
+  ;; (package-install 'org-contrib)
+  (package-install 'htmlize)
+  (setq user-full-name nil))
+
+;; org mode
+(require 'org)
 (require 'ox-publish)
-(dolist (pub-dir '("bin/" "env/" "dotfiles/" "docs/" "share/man/"))
-  (mkdir (format "%s" pub-dir) t))
+(require 'ox-man)
+
 (defun org-man-publish-to-man (plist filename pub-dir)
   "Publish an org file to MAN-PAGE.
 
@@ -19,17 +40,8 @@ Return output file name."
      plist
      man-dir)))
 
-
 (setq org-publish-project-alist
       (list
-       (list "org-html"
-             :base-directory "org/"
-             :base-extension "org"
-             :publishing-directory "docs/"
-             :recursive t
-             :publishing-function 'org-html-publish-to-html
-             :headline-levels 4
-             :auto-preamble t)
        (list "org-man"
              :base-directory "org/"
              :base-extension "org"
@@ -38,15 +50,40 @@ Return output file name."
              :publishing-function 'org-man-publish-to-man
              :headline-levels 4
              :auto-preamble t)
+       (list "org-notes"
+             :base-directory "org/"
+             :base-extension "org"
+             :publishing-directory "docs/"
+             :recursive t
+             :publishing-function 'org-html-publish-to-html
+             :headline-levels 4
+             :auto-preamble t)
        (list "org-static"
-             :base-directory "."
+             :base-directory "org/"
              :base-extension
              "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
              :publishing-directory "docs/"
              :recursive t
              :publishing-function 'org-publish-attachment)
        (list "org" :components
-             '("org-html" "org-static" "org-man"))))
-(org-publish "org" t)
-(dolist (litorg (directory-files "org/" nil ".org"))
-  (org-babel-tangle-file (format "%s/%s" "org/" litorg)))
+             '("org-notes" "org-static"))))
+
+(defun tangle-runcom ()
+  (dolist (pub-dir '("bin/" "env/" "dotfiles/" "docs/" "share/man/"))
+    (mkdir (format "%s" pub-dir) t))
+  (dolist (litorg (directory-files "org/" nil ".org"))
+    (org-babel-tangle-file (format "org/%s" litorg)))
+  (org-publish "org-man" t))
+
+(defun export-runcom ()
+  (mkdir "docs/" t)
+  (org-publish "org" t))
+
+(unless (getenv "CI_PROJECT_ID")
+  (tangle-runcom)
+  ;; (export-runcom)
+  )
+
+(provide 'publish)
+
+;;; publish.el ends here
