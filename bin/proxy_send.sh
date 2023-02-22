e009594c3716ab924cd77eb614ddb76e616b4b3c
Pradyumna Paranjape <pradyparanjpe@rediffmail.com>
2023-02-22 15:05:44 +0530

    Move mpv config from here to private config
    private config commit hash:
    a50cc2ae5ca627fef69e09b04dd0146a2c55b06a

--- bin/proxy_send.sh
+++ bin/proxy_send.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # -*- coding:utf-8; mode:shell-script -*-
 #
-# Copyright 2020, 2021 Pradyumna Paranjape
+# Copyright 2020-2023 Pradyumna Paranjape
 #
 # This file is part of Prady_runcom.
 #
