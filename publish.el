e9d51cf6b2f970809a0f3e518909c4361a5bf29a
Pradyumna Paranjape <6933745+pradyparanjpe@users.noreply.github.com>
2026-08-26 02:52:48 +0530

    Upstream changes in theme readtheorg

--- publish.el
+++ publish.el
@@ -29,13 +29,17 @@
 (setq org-html-head "
 <link rel=\"stylesheet\" href=\"https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css\">
 
-<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/htmlize.css\"/>
-<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/readtheorg.css\"/>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/htmlize.css\"/>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/readtheorg.css\"/>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/code-copy.css\"/>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/search.css\"/>
 
 <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js\"></script>
 <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js\"></script>
 <script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/lib/js/jquery.stickytableheaders.min.js\"></script>
-<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg_theme/js/readtheorg.js\"></script>
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg/js/search.js\"></script>
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg/js/readtheorg.js\"></script>
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg/js/code-copy.js\"></script>
 
 <style>pre.src{background:#343131;color:white;} </style>
 
